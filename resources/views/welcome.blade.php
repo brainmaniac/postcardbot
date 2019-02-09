4e859ab84f5da4c359e43ca3b9ea465e4c79a7ba
Brainmaniac <olof.jondelius@gmail.com>
2019-02-09 17:44:32 +0100

    testing to remove from blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
