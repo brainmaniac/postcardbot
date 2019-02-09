6871686ded228cb4117562daf8ce5789fd13d658
Mwthinker <mwthinker@yahoo.se>
2019-02-10 00:58:31 +0100

    Merge fix

--- resources/views/gps.blade.php
+++ resources/views/gps.blade.php
@@ -1,10 +1,7 @@
 @extends('master')
     @section('content')
         <div>
-<<<<<<< HEAD
-=======
 			<button id="gpsButton">Start GPS</button>
->>>>>>> fe457e4c8983e7fc235b8dfa3ae14e7dc823f57f
             <script>
                 startGps();
             </script>
