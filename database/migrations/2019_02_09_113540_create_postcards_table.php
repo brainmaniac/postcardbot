fd92654d1d36cf5dbcef47f8dcccaab5082435f9
Brainmaniac <olof.jondelius@gmail.com>
2019-02-09 12:45:10 +0100

    initial commit

---
+++ database/migrations/2019_02_09_113540_create_postcards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostcardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postcards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('lat');
+            $table->float('lng');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postcards');
+    }
+}
