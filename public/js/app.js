1c435d3c1390024f88d09f3b71c4036ecf3454c4
Brainmaniac <olof.jondelius@gmail.com>
2019-02-10 08:59:02 +0100

    quick fix gps

--- public/js/app.js
+++ public/js/app.js
@@ -36528,8 +36528,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/anders/Code/postcardbot/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/anders/Code/postcardbot/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/olofjondelius/code/postcardbot/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/olofjondelius/code/postcardbot/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/gps.blade.php
+++ resources/views/gps.blade.php
@@ -3,9 +3,37 @@
         <div>
 			<button id="gpsButton">Start GPS</button>
             <script>
-                setTimeout(function() {
-                    startGps();
-                }, 5000);                
+                // setTimeout(function() {
+                //     startGps();
+                // }, 5000);    
+                
+                var timeInterval = 1000;
+
+                function updateCurrentPosition() {
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function (position) {
+                            uploadGpsLocation(position.coords.latitude, position.coords.longitude);
+                        });
+                        setTimeout(updateCurrentPosition, timeInterval);
+                    } else {
+                        console.log("updateCurrentPosition failed");
+                    }
+                }
+
+                function uploadGpsLocation(latitude, longitude) {
+                    let url = "https://postcardbot.cyberandspace.se/addcoordinates/" + position.coords.latitude + "," + position.coords.longitude;
+                    console.log("get url");
+                    $.get( url, function( data ) {
+                        console.log("Upload sucessfull");        
+                    });
+                }
+
+                function startGps() {
+                    setTimeout(updateCurrentPosition, timeInterval);
+                }
+
+                startGps();
+
             </script>
         </div>
     @endsection
